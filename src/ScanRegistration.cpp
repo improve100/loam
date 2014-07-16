4d2f66a6ca87505232e0bd615c785c4f9587250b
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2014-07-16 10:10:41 +0200

    Removed some filtering.

--- src/ScanRegistration.cpp
+++ src/ScanRegistration.cpp
@@ -37,6 +37,10 @@ void ScanRegistration::addScan(pcl::PointCloud<pcl::PointXYZ>::Ptr laserCloudIn,
 	timeScanCur = timestamp;
 	timeLasted = timeScanCur - initTime;	
 	
+	// New temporary pc to hold some additional values
+	// h: ?
+	// s: Response of feature detector below
+	// v: ?
 	pcl::PointCloud<pcl::PointXYZHSV>::Ptr laserCloud(new pcl::PointCloud<pcl::PointXYZHSV>(cloudSize, 1));
 	for (int i = 0; i < cloudSize; i++)
 	{
@@ -49,7 +53,7 @@ void ScanRegistration::addScan(pcl::PointCloud<pcl::PointXYZ>::Ptr laserCloudIn,
 		cloudNeighborPicked[i] = 0;
 	}
 
-	// What is happening here?
+	// Feature point extraction (Section V-A)
 	for (int i = 5; i < cloudSize - 5; i++)
 	{
 		float diffX = laserCloud->points[i - 5].x + laserCloud->points[i - 4].x 
@@ -71,72 +75,7 @@ void ScanRegistration::addScan(pcl::PointCloud<pcl::PointXYZ>::Ptr laserCloudIn,
 			+ laserCloud->points[i + 3].z + laserCloud->points[i + 4].z
 			+ laserCloud->points[i + 5].z;
 
-		laserCloud->points[i].s = diffX * diffX + diffY * diffY + diffZ * diffZ;
-	}
-
-	for (int i = 5; i < cloudSize - 6; i++)
-	{
-		float diffX = laserCloud->points[i + 1].x - laserCloud->points[i].x;
-		float diffY = laserCloud->points[i + 1].y - laserCloud->points[i].y;
-		float diffZ = laserCloud->points[i + 1].z - laserCloud->points[i].z;
-		float diff = diffX * diffX + diffY * diffY + diffZ * diffZ;
-
-		if (diff > 0.05)
-		{
-			float depth1 = sqrt(laserCloud->points[i].x * laserCloud->points[i].x + 
-			laserCloud->points[i].y * laserCloud->points[i].y +
-			laserCloud->points[i].z * laserCloud->points[i].z);
-
-			float depth2 = sqrt(laserCloud->points[i + 1].x * laserCloud->points[i + 1].x + 
-			laserCloud->points[i + 1].y * laserCloud->points[i + 1].y +
-			laserCloud->points[i + 1].z * laserCloud->points[i + 1].z);
-
-			if (depth1 > depth2)
-			{
-				diffX = laserCloud->points[i + 1].x - laserCloud->points[i].x * depth2 / depth1;
-				diffY = laserCloud->points[i + 1].y - laserCloud->points[i].y * depth2 / depth1;
-				diffZ = laserCloud->points[i + 1].z - laserCloud->points[i].z * depth2 / depth1;
-
-				if (sqrt(diffX * diffX + diffY * diffY + diffZ * diffZ) / depth2 < 0.1)
-				{
-					cloudNeighborPicked[i - 5] = 1;
-					cloudNeighborPicked[i - 4] = 1;
-					cloudNeighborPicked[i - 3] = 1;
-					cloudNeighborPicked[i - 2] = 1;
-					cloudNeighborPicked[i - 1] = 1;
-					cloudNeighborPicked[i] = 1;
-				}
-			}else
-			{
-				diffX = laserCloud->points[i + 1].x * depth1 / depth2 - laserCloud->points[i].x;
-				diffY = laserCloud->points[i + 1].y * depth1 / depth2 - laserCloud->points[i].y;
-				diffZ = laserCloud->points[i + 1].z * depth1 / depth2 - laserCloud->points[i].z;
-
-				if (sqrt(diffX * diffX + diffY * diffY + diffZ * diffZ) / depth1 < 0.1)
-				{
-					cloudNeighborPicked[i + 1] = 1;
-					cloudNeighborPicked[i + 2] = 1;
-					cloudNeighborPicked[i + 3] = 1;
-					cloudNeighborPicked[i + 4] = 1;
-					cloudNeighborPicked[i + 5] = 1;
-					cloudNeighborPicked[i + 6] = 1;
-				}
-			}
-		}
-
-		float diffX2 = laserCloud->points[i].x - laserCloud->points[i - 1].x;
-		float diffY2 = laserCloud->points[i].y - laserCloud->points[i - 1].y;
-		float diffZ2 = laserCloud->points[i].z - laserCloud->points[i - 1].z;
-		float diff2 = diffX2 * diffX2 + diffY2 * diffY2 + diffZ2 * diffZ2;
-
-		float dis = laserCloud->points[i].x * laserCloud->points[i].x
-			+ laserCloud->points[i].y * laserCloud->points[i].y
-			+ laserCloud->points[i].z * laserCloud->points[i].z;
-
-		if (diff > (0.25 * 0.25) / (20 * 20) * dis && diff2 > (0.25 * 0.25) / (20 * 20) * dis)
-		{
-			cloudNeighborPicked[i] = 1;
-		}
+		laserCloud->points[i].s = diffX * diffX + diffY * diffY + diffZ * diffZ; // c-Value
 	}
 	
 	pcl::PointCloud<pcl::PointXYZHSV>::Ptr cornerPointsSharp(new pcl::PointCloud<pcl::PointXYZHSV>());
@@ -144,6 +83,8 @@ void ScanRegistration::addScan(pcl::PointCloud<pcl::PointXYZ>::Ptr laserCloudIn,
 	pcl::PointCloud<pcl::PointXYZHSV>::Ptr surfPointsFlat(new pcl::PointCloud<pcl::PointXYZHSV>());
 	pcl::PointCloud<pcl::PointXYZHSV>::Ptr surfPointsLessFlat(new pcl::PointCloud<pcl::PointXYZHSV>());
 
+
+	// Split scan in 4 regions
 	int startPoints[4] = {	5,
 								6 + int((cloudSize - 10) / 4.0),
 								6 + int((cloudSize - 10) / 2.0),
@@ -163,6 +104,7 @@ void ScanRegistration::addScan(pcl::PointCloud<pcl::PointXYZ>::Ptr laserCloudIn,
 		{
 			for (int k = j; k >= sp + 1; k--)
 			{
+				// Sort the points within each region based on their c-Value
 				if (laserCloud->points[cloudSortInd[k]].s < laserCloud->points[cloudSortInd[k - 1]].s)
 				{
 					int temp = cloudSortInd[k - 1];
